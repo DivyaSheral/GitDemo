55cbcc9994224a97cf53bc8dda3a5519ee4c5c31
divya <dvdasari91@gmail.com>
2020-07-15 01:17:13 +0530

    AddedDemoGitFiles

---
+++ src/Java/DemoForGit.java
@@ -0,0 +1,20 @@
+package Java;
+
+public class DemoForGit {
+	
+	public void add()
+	{
+		System.out.println("Addition");
+	}
+	
+	public void sub()
+	{
+		System.out.println("Subtract");
+	}
+	
+	public void mul()
+	{
+		System.out.println("Multiply");
+	}
+
+}
